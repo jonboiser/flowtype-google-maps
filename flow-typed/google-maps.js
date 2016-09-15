5d59edec92045f016bc4b86fdd54ddf64be6234e
Jonathan Boiser <jboiser@gmail.com>
2016-09-15 00:31:32 -0500

    copy MapOptions from typscript defs

--- flow-typed/google-maps.js
+++ flow-typed/google-maps.js
@@ -1,6 +1,4 @@
 /* @flow */
-// fitBounds(bounds:LatLngBounds|LatLngBoundsLiteral)
-
 declare class goog$LatLngBounds {
 
 }
@@ -29,7 +27,50 @@ declare class goog$MVCObject {
   unbindAll(): void;
 }
 
+declare interface foo {
+  bar: number,
+  baz: string,
+}
+
+declare type goog$MapOptions = {
+  backgroundColor?: string;
+  center?: goog$LatLng | goog$LatLngLiteral;
+  disableDefaultUI?: boolean;
+  clickableIcons?: boolean;
+  disableDoubleClickZoom?: boolean;
+  draggable?: boolean;
+  draggableCursor?: string;
+  draggingCursor?: string;
+  heading?: number;
+  keyboardShortcuts?: boolean;
+  mapMaker?: boolean;
+  mapTypeControl?: boolean;
+  mapTypeControlOptions?: MapTypeControlOptions;
+  mapTypeId?: MapTypeId;
+  maxZoom?: number;
+  minZoom?: number;
+  noClear?: boolean;
+  overviewMapControl?: boolean;
+  overviewMapControlOptions?: OverviewMapControlOptions;
+  panControl?: boolean;
+  panControlOptions?: PanControlOptions;
+  rotateControl?: boolean;
+  rotateControlOptions?: RotateControlOptions;
+  scaleControl?: boolean;
+  scaleControlOptions?: ScaleControlOptions;
+  scrollwheel?: boolean;
+  streetView?: StreetViewPanorama;
+  streetViewControl?: boolean;
+  streetViewControlOptions?: StreetViewControlOptions;
+  styles?: MapTypeStyle[];
+  tilt?: number;
+  zoom?: number;
+  zoomControl?: boolean;
+  zoomControlOptions?: ZoomControlOptions;
+}
+
 declare class goog$Map extends goog$MVCObject {
+  constructor(mapDiv: Element, opts?: Object): void;
   fitBounds(bounds: goog$LatLngBounds | goog$LatLngBoundsLiteral): void;
   getBounds(): goog$LatLngBounds;
   getCenter(): goog$LatLng;
@@ -48,7 +89,7 @@ declare class goog$Map extends goog$MVCObject {
   setClickableIcons(value: boolean): void;
   setHeading(heading: number): void;
   setMapTypeId(mapTypeId: string): void;
-  setOptions(options: Object): void;
+  setOptions(options: goog$MapOptions): void;
   setStreetView(panorama: Object): void;
   setTilt(tilt: number): void;
   setZoom(zoom: number): void;
--- test_google-maps.js
+++ test_google-maps.js
@@ -2,10 +2,12 @@
 google.maps;
 google.maps.Map;
 
-const map = new google.maps.Map();
+const map = new google.maps.Map(document.getElementById('map'));
 
 map.addListener('yo', () => {})
 
+// interface doesn't work with object literal
+map.foobar({ bar: 1, ad: ''});
 map.fitBounds({ lat: 123, lng: 123 });
 
 map.get('foobar');
